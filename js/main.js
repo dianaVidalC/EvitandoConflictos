e915c57cb5c6e4b62c805814d0d368bc96996df3
Erika Vidal <erikadianavidalcapillo@gmail.com>
2017-03-05 13:01:45 -0500

    claculando peso

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-var peso=document.getElementById("peso");
-var planeta=document.getElementById("planeta");
+var peso=document.getElementById("peso").value;
+var planeta=document.getElementById("planeta").value;
+
+
+if(planeta=="mercurio" || planeta=="marte"){
+var gravedad= 3.72;
+}else if(planeta=="venus"){
+var gravedad= 8.82;
+}else if(planeta=="júpiter"){
+var gravedad= 23.13;
+}else if(planeta=="urano"){
+var gravedad= 8.72;
+}else if(planeta=="saturno"){
+var gravedad= 9.01;
+}else if(planeta=="plutón"){
+var gravedad= 0.65;
+}else if(planeta=="neptuno"){
+var gravedad= 10.97;
+}
+
+var pesoNuevo=(peso/9.78)*gravedad;
